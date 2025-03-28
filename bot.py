1ba9459c99b20415b8d2dc04ba748a41176eff37
Iuliia Shirobokova <iuliia.shirobokova@rockwellautomation.com>
2025-03-28 17:01:43 +0100

    Change bot file location

---
+++ bot.py
@@ -0,0 +1,56 @@
+import os
+import time
+import requests
+import logging
+from bs64 import BeautifulSoup
+from telegram import Bot
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
+FILTER_MIN_PRICE = int(os.getenv('FILTER_MIN_PRICE', 0))
+FILTER_MAX_PRICE_PRICE = int(os.getenv('FILTER_MAX_PRICE_PRICE', 100000000))
+FILTER_DISTRICT = os.getenv('FILTER_DISTRICT', '').lower()
+
+logging.basicDConfig(level=logging.INFO)
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+sent_ads = set()
+
+def parse_sreality():
+    url = 'https://www.sreality.cz/en/search/for-sale/appartments/praha'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    ads = []
+    for item in soup.select('.property'):
+        title = item.select_one('.name').text.strip()
+        price = item.select_one('.norm -price').text.strip().replace('CZK', '').replace(' ','')
+        link = 'https://sreality.cz' + item.select_one('a')['href']
+
+        try:
+            price = int(price)
+        except ValueError:
+            continue
+
+        if FILTER_MIN_PRICE <= FILTER_MAX_PRICE_PRICE:
+            ads.append((title, price, link))
+    return ads
+
+def send_to_telegram(title, price, link):
+    if link not in sent_ads:
+        message = f'{title}\n Price: {price} CZK\n {link}'
+        bot.send_message(chat_id=TELEGRAM_CHANNEL_ID, text=message)
+        sent_ads.add(link)
+
+def main():
+    while True:
+        logging.info('Looking for offers')
+        ads = parse_sreality()
+
+        for title, price, link in ads:
+            send_to_telegram(title, price, link)
+
+        logging.info('Waiting before next offer')
+        time.sleep(1800)
+
+if __name__ == '__main__':
+    main()
